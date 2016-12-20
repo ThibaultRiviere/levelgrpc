fe6a8d02b657a5f1ee45ed9d28f33cada9788ae6
Thibault Riviere <thibault.riviere@scality.com>
2016-12-20 22:09:19 +0100

    [SERVER](RF): seperate server
    - NewServer func take grpc Server for make it configurable
    - Server func take a net Listener for make it configurable
    - Seperate db interaction in functions (_get/_put)

--- server/server.go
+++ server/server.go
@@ -1,4 +1,4 @@
-package main
+package server
 
 import (
 	"fmt"
@@ -6,20 +6,32 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/levelgrpc/proto"
-	"log"
 	"net"
 )
 
-type LevelDB struct {
-	db *leveldb.DB
+type Server struct {
+	db     *leveldb.DB
+	server *grpc.Server
 }
 
-func (lvl *LevelDB) Put(c context.Context, m *pb.PutObject) (*pb.Response, error) {
-	key := m.GetDatabase() + "/" + m.GetKey()
-	err := lvl.db.Put([]byte(key), []byte(m.GetValue()), nil)
+func (s *Server) _put(db string, key string, value string) error {
+	k := db + "/" + key
+	//fmt.Println("Putting : ", k, ":", value)
+	return s.db.Put([]byte(k), []byte(value), nil)
+}
 
-	//fmt.Println("Putting : ", key, ":", m.GetValue())
+func (s *Server) _get(db string, key string) (string, error) {
+	k := db + "/" + key
+	val, err := s.db.Get([]byte(k), nil)
+	if err != nil {
+		return "", err
+	}
+	//fmt.Println("Getting : ", key, ":", value)
+	return string(val), nil
+}
 
+func (s *Server) Put(c context.Context, m *pb.PutObject) (*pb.Response, error) {
+	err := s._put(m.GetDatabase(), m.GetKey(), m.GetValue())
 	if err != nil {
 		fmt.Println("put error : ", err)
 		return &pb.Response{"nok", ""}, err
@@ -27,20 +39,22 @@ func (lvl *LevelDB) Put(c context.Context, m *pb.PutObject) (*pb.Response, error
 	return &pb.Response{"ok", ""}, nil
 }
 
-func (lvl *LevelDB) Get(c context.Context, m *pb.GetObject) (*pb.Response, error) {
-	key := m.GetDatabase() + "/" + m.GetKey()
-	data, err := lvl.db.Get([]byte(key), nil)
-
+func (s *Server) Get(c context.Context, m *pb.GetObject) (*pb.Response, error) {
+	data, err := s._get(m.GetDatabase(), m.GetKey())
 	if err != nil {
 		fmt.Println("get error : ", err)
 		return &pb.Response{"nok", ""}, err
 	}
-	value := string(data)
-	//fmt.Println("Getting : ", key, ":", value)
-	return &pb.Response{"ok", value}, nil
+	return &pb.Response{"ok", data}, nil
 }
 
-func NewLevelDB() (*LevelDB, error) {
+func (s *Server) Serve(lis net.Listener) error {
+	pb.RegisterLevelDBServer(s.server, s)
+	s.server.Serve(lis)
+	return nil
+}
+
+func NewServer() (*Server, error) {
 	// Open the database
 	// TODO need to be configurable
 	db, err := leveldb.OpenFile("/tmp/levelgrpc", nil)
@@ -48,22 +62,5 @@ func NewLevelDB() (*LevelDB, error) {
 		fmt.Println("Failed to open the database")
 		return nil, err
 	}
-	return &LevelDB{db}, nil
-}
-
-func main() {
-	lis, err := net.Listen("tcp", ":4242")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-		return
-	}
-	grpcServer := grpc.NewServer()
-	levelgrpc, err := NewLevelDB()
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-		return
-	}
-	pb.RegisterLevelDBServer(grpcServer, levelgrpc)
-	grpcServer.Serve(lis)
-	return
+	return &Server{db, grpc.NewServer()}, nil
 }
